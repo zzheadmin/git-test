97c1a74667933cabc7fc3c88c9aa9e2de1bcff51
hey <hey@zzz.com>
2021-07-12 20:50:45 +0800

    first commit

---
+++ src/main/java/com/hey/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hey.git;
+
+/**
+ * @author he
+ * @date 2021/7/12 20:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
