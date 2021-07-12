4a13a3e983a2fa672a1133b98c470d737caeb511
hey <hey@zzz.com>
2021-07-12 21:10:06 +0800

    hot-fix commit1

--- src/main/java/com/hey/git/GitTest.java
+++ src/main/java/com/hey/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
